f3345c483131f349755ad803522e36e304c1241d
Shantanu <shantanut710@gmail.com>
2021-03-04 16:03:36 +0530

    implementation of pod CLTypingLabel

--- ChatApp/Controller/WelcomeViewController.swift
+++ ChatApp/Controller/WelcomeViewController.swift
@@ -5,25 +5,29 @@
 //  Created by Shantanu on 31/12/20.
 //
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
-
-    @IBOutlet weak var titleLabel: UILabel!
+    
+//    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Animation Code STR
-        titleLabel.text = ""
-        var charIndex = 0.0
-        let titleText = "⚡️ChitChat"
-        for letter in titleText {
-            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
-                self.titleLabel.text?.append(letter)
-            }
-            charIndex += 1
-        }
-        // Animation Code END
+        titleLabel.text = "⚡️ChitChat"
+        // This is old method (withoud CLTypingLabel Pod method).
+//        // Animation Code STR
+//        titleLabel.text = ""
+//        var charIndex = 0.0
+//        let titleText = "⚡️ChitChat"
+//        for letter in titleText {
+//            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
+//                self.titleLabel.text?.append(letter)
+//            }
+//            charIndex += 1
+//        }
+//        // Animation Code END
 
     }
       
--- Podfile
+++ Podfile
@@ -1,10 +1,10 @@
-platform :ios, '9.0'
+platform :ios, '10.0'
 
 target 'ChatApp' do
   use_frameworks!
 
   # Pods for ChatApp
 
-    pod 'CLTypingLabel'
-    
+    pod 'CLTypingLabel', '~> 0.4.0'
+
 end
